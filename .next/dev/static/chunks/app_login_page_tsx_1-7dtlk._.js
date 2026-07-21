cf6adf7479d940ea36a70118178d6a58239d06e3
extrovrtd <esocloo001@st.ug.edu.gh>
2026-07-21 09:52:26 +0000

    Revert "feat: implement login page and dashboard (US-01) with Supabase Auth"

--- .next/dev/static/chunks/app_login_page_tsx_1-7dtlk._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_0a2ldrx._.js","static/chunks/app_login_page_tsx_1wi-ux9._.js"],
-    source: "dynamic"
-});
