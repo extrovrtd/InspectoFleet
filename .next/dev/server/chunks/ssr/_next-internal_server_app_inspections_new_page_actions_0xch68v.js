46bb5b9133979abc8d238a4130b81a6b1ba2d061
extrovrtd <esocloo001@st.ug.edu.gh>
2026-08-06 00:50:17 +0000

    fix: remove .next build cache from git tracking

--- .next/dev/server/chunks/ssr/_next-internal_server_app_inspections_new_page_actions_0xch68v.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/inspections/new/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_inspections_new_page_actions_0xch68v.js.map
