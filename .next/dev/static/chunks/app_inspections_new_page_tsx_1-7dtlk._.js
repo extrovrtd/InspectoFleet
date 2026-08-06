703c426cdb3409817cb4233f766ac30257c6f592
extrovrtd <esocloo001@st.ug.edu.gh>
2026-08-06 00:25:30 +0000

    fix: remove .next cache from main branch

---
+++ .next/dev/static/chunks/app_inspections_new_page_tsx_1-7dtlk._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/node_modules_0a2ldrx._.js","static/chunks/app_inspections_new_page_tsx_07st-tc._.js"],
+    source: "dynamic"
+});
